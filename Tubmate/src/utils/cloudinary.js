e6f172c938653812874c2b28ea8fd41a17e90684
Gaurav Singh <gksingh947@gmail.com>
2024-11-08 13:36:42 +0530

    Cloudinary Bug Fix

--- Tubmate/src/utils/cloudinary.js
+++ Tubmate/src/utils/cloudinary.js
@@ -1,16 +1,21 @@
+import dotenv from "dotenv"
+dotenv.config()
 import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 
 cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_KEY, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
+  cloud_name:process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key:process.env.CLOUDINARY_API_KEY, 
+  api_secret:process.env.CLOUDINARY_API_SECRET 
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if (!localFilePath) return null
+        if (!localFilePath || !fs.existsSync(localFilePath)) {
+      console.error("Local file path is invalid or file does not exist:", localFilePath);
+      return null;
+    }
         //upload the file on cloudinary
         const response = await cloudinary.uploader.upload(localFilePath, {
             resource_type: "auto"
