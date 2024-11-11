f79a570b4ca51069feda64f85bee53608246573f
Gaurav Singh <gksingh947@gmail.com>
2024-11-11 14:16:50 +0530

    Token Generate Fix

--- Tubmate/src/controllers/userController.js
+++ Tubmate/src/controllers/userController.js
@@ -4,18 +4,20 @@ import {User} from '../models/userModel.js';
 import {uploadOnCloudinary} from '../utils/cloudinary.js';
 import {ApiResponse} from '../utils/ApiResponse.js';
 
-const generateAccessAndRefreshToken=async(userId)=>{
+const generateAccessAndRefereshTokens = async(userId) =>{
   try {
-    const user= await User.findById(userId);
-    const accessToken= await user.generateAccessToken();
-    const refreshToken=awaituser.generateRefreshToken();
-    user.refreshToken=refreshToken;
-    await user.save({validateBeforeSave:false});
+      const user = await User.findById(userId)
+      const accessToken = user.generateAccessToken()
+      const refreshToken = user.generateRefreshToken()
 
-    return {accessToken,refreshToken};
+      user.refreshToken = refreshToken
+      await user.save({ validateBeforeSave: false })
+
+      return {accessToken, refreshToken}
+
 
   } catch (error) {
-    throw new ApiError(500,"Something went wrong while generating tokens");
+      throw new ApiError(500, "Something went wrong while generating referesh and access token")
   }
 }
 
@@ -100,7 +102,7 @@ export const loginUser=asyncHandler(async(req,res)=>{
    if(!isPasswordValid){
       throw new ApiError(401,"Invalid password");
    }
-  const {accessToken,refreshToken}=await generateAccessAndRefreshToken(user._id);
+  const {accessToken,refreshToken}=await generateAccessAndRefereshTokens(user._id);
 
   const loggedInUser=await User.findById(user._id).select("-password -refreshToken");
    
